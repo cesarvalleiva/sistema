c222c72515ef77ad01b8c00ff40b0b64a452d110
cesarvalleiva <cesarvalleiva@gmail.com>
2020-03-23 08:03:00 -0300

    initial configuration

---
+++ index.js
@@ -0,0 +1,19 @@
+
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')))
+
+app.set('port',process.env.PORT || 3000);
+app.listen(app.get('port'), () => {
+    console.log('Server on port' + app.get('port'));
+});
